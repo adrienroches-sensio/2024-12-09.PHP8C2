227b29104d9084c621462b9a705a6a06ff579146
Adrien Roches <adrien@roc-it.tech>
2024-12-09 10:35:50 +0100

    Mark password arguments "Sensitive"

--- src/Member.php
+++ src/Member.php
@@ -4,13 +4,20 @@ class Member
 {
     public function __construct(
         private string $login,
+
+        #[SensitiveParameter]
         private string $password,
+
         private int $age,
     ) {
     }
 
-    public function auth(string $login, string $password): bool
-    {
+    public function auth(
+        string $login,
+
+        #[SensitiveParameter]
+        string $password
+    ): bool {
         return $this->login === $login && $this->password === $password;
     }
 }
