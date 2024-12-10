03a95c371ca722fab9271f74448c995e33031fa3
Adrien Roches <adrien@roc-it.tech>
2024-12-10 16:39:26 +0100

    Final tests

--- tests/MemberLevelTest.php
+++ tests/MemberLevelTest.php
@@ -5,18 +5,28 @@ declare(strict_types=1);
 namespace Test;
 
 use App\MemberLevel;
+use Generator;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(MemberLevel::class)]
 class MemberLevelTest extends TestCase
 {
-    public function testAllCasesHaveALabel(): void
+    public static function memberLevelProvider(): Generator
     {
-        foreach (MemberLevel::cases() as $case) {
-            $case->label();
-
-            $this->addToAssertionCount(1);
+        foreach (MemberLevel::cases() as $level) {
+            yield $level->name => [$level];
         }
+    }
+
+    #[TestDox('"$_dataName" have a label')]
+    #[DataProvider('memberLevelProvider')]
+    public function testCaseHaveALabel(MemberLevel $level): void
+    {
+        $level->label();
+
+        $this->addToAssertionCount(1);
     }
 }
